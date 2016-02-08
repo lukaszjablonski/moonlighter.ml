ccca38f4163d6996e45ee28e1c1340342227000f
lukaszjablonski <lukasz.jablonski@wp.eu>
2016-02-08 11:43:47 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 /**
- *
- monlighter.ml js
+ * monlighter.ml js
  */
+
+/* Scroll prgoress bar [https://css-tricks.com/reading-position-indicator/] */
+$(document).on('ready', function() {  
+ var winHeight = $(window).height(), 
+     docHeight = $(document).height(),
+     progressBar = $('progress'),
+     max, value;
+
+ /* Set the max scrollable area */
+ max = docHeight - winHeight;
+ progressBar.attr('max', max);
+
+ $(document).on('scroll', function(){
+  value = $(window).scrollTop();
+  progressBar.attr('value', value);
+ });
+ 
+ $(window).on('resize', function() {
+  winHeight = $(window).height(),
+  docHeight = $(document).height();
+  max = docHeight - winHeight;
+  progressBar.attr('max', max);
+  value =  $(window).scrollTop();
+  progressBar.attr('value', value);
+ });
+ 
+});
