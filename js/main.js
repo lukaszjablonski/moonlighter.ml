2f85e21056a37c16f1d53f3045f3d82167c7a45f
lukaszjablonski <lukasz.jablonski@wp.eu>
2016-02-12 10:22:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,9 +43,5 @@ function animateHeight(element) {
  curHeight = el.height(),
  autoHeight = el.css('height', 'auto').height();
  el.height(curHeight).animate({height: autoHeight}, 300);
- setTimeout(
-  function() 
-  {
-    $('progress').trigger('resize')
-  }, 305);
+ setTimeout($('progress').trigger('resize'), 305);
 }
