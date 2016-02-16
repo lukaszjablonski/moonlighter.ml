6e431d05734fc1333a9e9dc2292a0a00c97510d0
lukaszjablonski <lukasz.jablonski@wp.eu>
2016-02-16 10:38:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,13 +3,12 @@
  * by Lukasz Jablonski [e-eightyseven.com]
  */
 
-var progressBar = $('progress'); // progress bar element
 $(document).on('ready', function() {
  
  /* Scroll prgoress bar [https://css-tricks.com/reading-position-indicator/] */
  var winHeight = $(window).height(), 
  docHeight = $(document).height(),
- //progressBar = $('progress'), // progress bar element
+ progressBar = $('progress'), // progress bar element
  max, value;
 
  // Set the max scrollable area
@@ -38,17 +37,17 @@ $(document).on('ready', function() {
   resizeProgressBar();
  });
  
-});
-
-function animateHeight(element,speed = 300) {
- var el = $(element),
- curHeight = el.height(),
- autoHeight = el.css('height', 'auto').height();
- el.height(curHeight).animate({height: autoHeight}, speed);
+ function animateHeight(element,speed = 300) {
+  var el = $(element),
+  curHeight = el.height(),
+  autoHeight = el.css('height', 'auto').height();
+  el.height(curHeight).animate({height: autoHeight}, speed);
  
- // change progressBar only after animation
-  //setTimeout(function(){progressBar.trigger('resize')}, speed+5);
- // change progressBar during animation
-  var ani=setInterval(function(){ progressBar.trigger('resize')}, 1);
-  setTimeout(function(){clearInterval(ani)}, speed+5);
-}
+  // change progressBar only after animation
+   //setTimeout(function(){progressBar.trigger('resize')}, speed+5);
+  // change progressBar during animation
+   var ani=setInterval(function(){ progressBar.trigger('resize')}, 1);
+   setTimeout(function(){clearInterval(ani)}, speed+5);
+ }
+ 
+});
