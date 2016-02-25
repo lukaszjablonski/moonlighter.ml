26f7780f0b4615a6eeced4b386eff7bd76d211ed
lukaszjablonski <lukasz.jablonski@wp.eu>
2016-02-25 12:46:00 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@
 
 $(window).bind("load", function() {
 // run after entire page has loaded
-  if ($('#cookieChoiceInfo').length>0) $('html').css('margin-top',$('html').css('margin-top')+$('#cookieChoiceInfo').outerHeight();
+  if ($('#cookieChoiceInfo').length>0) $('html').css('margin-top',$('html').css('margin-top')+$('#cookieChoiceInfo').outerHeight());
 });
 
 function animateHeight(element,speed) {
