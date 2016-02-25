81e603ecd0517909a2b296ca7d81a7add4b392a6
lukaszjablonski <lukasz.jablonski@wp.eu>
2016-02-25 15:37:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -59,8 +59,8 @@ $(window).bind("load", function() {
    //$('#cookieChoiceDismiss').removeAttr('onclick'); // remove original event
    $('#cookieChoiceDismiss').click(function() {
     $('html').animate({'margin-top': '-='+newMargin}, alertAnimationSpeed);
-    $('#cookieChoiceDismiss').animate({'margin-top': '-='+newMargin}, alertAnimationSpeed);
-    setTimeout(function(){_dismissLinkClick}, alertAnimationSpeed+50); // call original event after animation
+    //$('#cookieChoiceDismiss').animate({'margin-top': '-='+newMargin}, alertAnimationSpeed);
+    //setTimeout(function(){_dismissLinkClick}, alertAnimationSpeed+50); // call original event after animation
    });
   }
 });
