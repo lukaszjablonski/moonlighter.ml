daae64a9f83446b551242f1d23f74c7882ed8e3c
lukaszjablonski <lukasz.jablonski@wp.eu>
2016-02-25 10:10:52 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@
   });
   
   resizeProgressBar(); // run once (important on page refresh)
-  if($('#cookieChoiceInfo')>0) $('body').css('margin-top','150px');
+  if($('#cookieChoiceInfo').length>0) $('body').css('margin-top','150px');
  
  });
 // SPB END
